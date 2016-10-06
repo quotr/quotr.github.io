8ccf188c0fac5b69fa4e2347951cf23e2a621eda
afable <afablee@gmail.com>
2016-10-06 05:04:26 -0700

    added basic website functionality to html, css, js files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview The main js implementation for quotr.github.io, 2016.
+ * @author afablee@gmail.com, @superafable
+ */
+
+'use strict';
+
+// document and window ready functions (test which loads first)
+// window.onload = function() { console.log("LOADED window.onload..."); };
+// (function() { console.log("LOADED IIFE.."); })();
+// $(document).ready(function() { console.log("LOADED $(document).ready..."); });
+
+(function($) {
+	console.log('anonymous $.iife done.');
+
+})(jQuery);
