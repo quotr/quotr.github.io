1b2b6fa59af105c0118227c5a21fe4bfbced0608
afable <afablee@gmail.com>
2016-10-06 21:30:11 -0700

    bkg hsl value now changes .on(mousemove) & .on(click)

--- js/main.js
+++ js/main.js
@@ -11,6 +11,44 @@
 // $(document).ready(function() { console.log("LOADED $(document).ready..."); });
 
 (function($) {
-	console.log('anonymous $.iife done.');
+	
+	// global background variables 
+	var g_x = 0;
+	var g_y = 0;
+	var x_grad = 0;
+	var y_grad = 0;
+	var GRADIENT_INCREMENT = 1;
+	var lightness = 0;
+
+	// mousemoves change background colour
+	$(document).on('mousemove.bkg', function(e) {
+		// capture x (left & right) movements & sign 
+		var x = e.clientX;
+		var x_left = ( x < g_x )? true : false;
+		var x_right = ( x > g_x )? true : false;
+		var x_sig = ( x_left )? -1 : ( x_right )? 1 : 0;
+		g_x = x;
+		x_grad += x_sig * GRADIENT_INCREMENT;
+
+		// capture y (up & down) movements & sign
+		var y = e.clientY;
+		var y_up = ( y < g_y )? true : false;
+		var y_down = ( y > g_y )? true : false;
+		var y_sig = ( y_up )? -1 : ( y_down )? 1 : 0;
+		g_y = y;
+		y_grad += y_sig * GRADIENT_INCREMENT;
+		
+		var grad = x_grad + y_grad;
+		$('body').css('background-image', 'linear-gradient(to right top, hsla('+grad+', 100%, 70%, 0.66), hsla('+(grad+180)+', 100%, 70%, 0.66))');
+	});
+
+	// mouseclicks change background light
+	$(document).on('click.bkg', function(e) {
+		lightness = Math.random()*100 >> 0;
+		var colour = Math.random()*360 >> 0;
+		$('body').css('background-color', 'hsl('+colour+', 100%, '+lightness+'%)');
+	});
+
+
 
 })(jQuery);
