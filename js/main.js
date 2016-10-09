f09e8e8103d14aaa3bc1b90a83f2cd584980eaad
afable <afablee@gmail.com>
2016-10-09 07:26:15 -0700

    inner content now fades out/in on #new-quote (if not already animating). fixed src code line length. started adding in undertale quotes

--- js/main.js
+++ js/main.js
@@ -26,18 +26,40 @@
 	var g_animating = false;
 	var g_fontColor = '#fff';
 	var FONT_LIGHT_THRESHOLD = 60;
-	var g_arrQuotes = [];	
-
 	// build the quote library
-	for ( let i = 0; i < 10; ++i ) {
-		g_arrQuotes.push({'quote': 'This is a quote #'+i, 'person': 'person #'+i});
-	}
+	var g_arrQuotes = [
+		{	'quote': 'This town doesn\'t have a mayor. But if there\'s ever ' +
+				'problem, a skeleton will tell a fish lady about it. ' +
+				'Thaaaaaat\'s politics!',
+			'person': 'Snowdin Bear' },
+		{	'quote': 'This is a quote that I am making up because I am a ' +
+				'maker-upper.',
+			'person': 'Me'},
+		{	'quote': 'If I die today, then it means nothing.',
+			'person': 'Sam Pepper'},
+		{	'quote': 'If you don\'t like your situation, change it. If you ' +
+				'can\'t, change your attitude. Don\'t complain.',
+			'person': 'Maya Angelou'},
+		{	'quote': 'As I walk through the valley and I look at my life, ' +
+				'I think I\'m some kind of really obscure kind of foolish ' +
+				'human being.',
+			'person': 'Monkey Moniker'}
+	];	
 
 	// grab a new quote on #new-quote button click
 	$('#new-quote').on('click.getQuote', function() {
+		// do nothing if still animating 
+		if ( $(':animated').length ) {
+			return;
+		}
 		var index = ( Math.random() * g_arrQuotes.length ) >> 0;
-		$('#quote').text(g_arrQuotes[index].quote);
-		$('#person').text(g_arrQuotes[index].person);
+		// fadeout, change quote, fadein
+		$('.inner').animate({'opacity': 0}, 'slow', function() {
+			$('#quote').html('<i class="fa fa-quote-left" ' + 
+				'aria-hidden="true"></i> ' + g_arrQuotes[index].quote);
+			$('#person').html('&mdash; ' + g_arrQuotes[index].person);
+			$(this).animate({'opacity': 1}, 'slow');
+		})
 	});
 
 	// mousemoves change background colour
@@ -59,7 +81,9 @@
 		g_yGrad += y_sig * GRADIENT_INCREMENT;
 		
 		var grad = g_xGrad + g_yGrad;
-		$('body').css('background-image', 'linear-gradient(to right top, hsla('+grad+', 100%, 70%, 0.66), hsla('+(grad+180)+', 100%, 70%, 0.66))');
+		$('body').css('background-image',
+			'linear-gradient(to right top, hsla('+grad+', 100%, 70%, 0.66), ' +
+			'hsla('+(grad+180)+', 100%, 70%, 0.66))');
 	});
 
 	// mouseclicks change background light
@@ -70,13 +94,15 @@
 		g_animating = true;
 		g_lightness = Math.random()*100 >> 0;
 		var colour = Math.random()*360 >> 0;
-		$('body').animate({'background-color': 'hsl('+colour+', 100%, '+g_lightness+'%)'}, 'slow', function() {
-			g_animating = false;
+		$('body').animate({'background-color': 'hsl('+colour+', 100%, ' +
+			g_lightness+'%)'}, 'slow', function() {
+				g_animating = false;
 		} );
 
 		// if g_lightness > 75%, change font for better UX
 		g_fontColor = ( g_lightness > FONT_LIGHT_THRESHOLD )? '#333' : '#fff';
-		$('.inner__text-color').animate({'color': g_fontColor, 'border-color': g_fontColor}, 'slow');	
+		$('.inner__text-color').animate({'color': g_fontColor,
+			'border-color': g_fontColor}, 'slow');	
 		de&&bug('g_lightness='+g_lightness+', g_fontColor='+g_fontColor);
 	});
 
